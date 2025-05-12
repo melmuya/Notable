3266c94158405f4b0bcfc6bd2cbadfb1b373a083
Mel <melchizedek.maranga@gmail.com>
2025-05-12 13:00:23 +0300

    Initialized React app using vite in the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1>Notable</h1>
+      <p>Welcome to your note-taking app!</p>
+      <footer>© 2023 Notable. All rights reserved.</footer>
+    </div>
+    
+  )
+}
+
+export default App
