c7e96ad968956c24d697337fa9934d8955541af0
Mel <melchizedek.maranga@gmail.com>
2025-05-14 09:37:15 +0300

    Set up Axios instance with JWT token handling for authenticated API requests

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:5000/api"
+
+const axiosInstance = axios.create({ baseURL: BASE_URL})
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
