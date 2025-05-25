8468b2e7e9b95c6952252f5ec3c85d57fa04e8cd
Mel <melchizedek.maranga@gmail.com>
2025-05-25 15:00:22 +0300

    Modified Dashboard styles, fixed bug in PrivateRoute, installed gunicorn and modified requirements.txt, and finally added a Procfile

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
 
-  return token ? children : <Navigate to="/login" />;
+  return token ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
