ae7e6831544323cd9f536a23625753be0f34b578
Mel <melchizedek.maranga@gmail.com>
2025-05-26 14:13:48 +0300

    debug axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.VITE_APP_API_URL;
 
 const axiosInstance = axios.create({ baseURL: BASE_URL})
 
